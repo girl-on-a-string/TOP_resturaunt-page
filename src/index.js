322e2e9e9cf0eea079578b03e1d461fe5e6b8fc6
girl-on-a-string <frenchfries55@protonmail.com>
2024-12-10 12:38:38 -0600

    created mainpage js and imported into index

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import { mainPageContent } from "./mainpage";
+
+console.log("hello from index");
+
+//this page will handle all the other pages displays and tab action
---
+++ src/mainpage.js
@@ -0,0 +1,7 @@
+// this page will display the default tab / content on the page
+
+const mainPageContent = (() => {
+    console.log("hello from mainpage");
+})();
+
+export {mainPageContent};
