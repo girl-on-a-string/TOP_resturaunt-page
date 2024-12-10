3878e45fd091e334aed2ba696c617faafae59cf7
girl-on-a-string <frenchfries55@protonmail.com>
2024-12-10 17:54:55 -0600

    imported css

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 import { mainPageContent } from "./mainpage";
+import "./styles.css";
 
 console.log("hello from index");
+
+const DOMelements = (() => {
+    let welcomeBanner = document.createElement("div");
+})();
+
+export {DOMelements};
 
 //this page will handle all the other pages displays and tab action
