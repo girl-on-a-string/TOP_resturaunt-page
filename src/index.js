7e406187118eceb7d4644382545d1355698b4aa1
girl-on-a-string <frenchfries55@protonmail.com>
2024-12-12 10:37:21 -0600

    welcome banner now displaying mainpage content properly

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+//this page will handle all the other pages displays and tab action
+
 import { mainPageContent } from "./mainpage";
 import "./styles.css";
 
@@ -5,8 +7,19 @@ console.log("hello from index");
 
 const DOMelements = (() => {
     let welcomeBanner = document.createElement("div");
+    welcomeBanner.setAttribute("id", "welcomeBanner");
+
+    let contentDiv = document.getElementById("content");
+    contentDiv.appendChild(welcomeBanner);
+
+    // mainPageContent();
+
+    return {
+        welcomeBanner, contentDiv
+    }
 })();
+
+mainPageContent()
 
 export {DOMelements};
 
-//this page will handle all the other pages displays and tab action
--- src/mainpage.js
+++ src/mainpage.js
@@ -1,7 +1,11 @@
 // this page will display the default tab / content on the page
 
-const mainPageContent = (() => {
+import { DOMelements } from ".";
+
+const mainPageContent = () => {
     console.log("hello from mainpage");
-})();
+
+    DOMelements.welcomeBanner.innerText = "Welcome to my super cool swag website, Currently on the home page rn."
+};
 
 export {mainPageContent};
