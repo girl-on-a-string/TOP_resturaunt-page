ab4b3b41c4b057fab856e02b2dd6e729f3524c1d
girl-on-a-string <frenchfries55@protonmail.com>
2024-12-16 09:54:10 -0600

    downloaded bg images, added them to root. Corrected mainpage welcome banner txt

--- src/mainpage.js
+++ src/mainpage.js
@@ -1,11 +1,18 @@
 // this page will display the default tab / content on the page
 
 import { DOMelements } from ".";
+// import mainPageImg from ".//alexandre-topolewski-95izPCyzmrc-unsplash.jpg";
 
 const mainPageContent = () => {
     console.log("hello from mainpage");
 
-    DOMelements.welcomeBanner.innerText = "Welcome to my super cool swag website, Currently on the home page rn."
+    const bgImg = document.createElement("img");
+    bgImg.src = mainPageImg;
+
+    DOMelements.welcomeBanner.innerText = "Welcome to my super cool swag resturaunt, Currently on the home page rn."
+    // DOMelements.welcomeBanner.style.backgroundImage = "url('src/alexandre-topolewski-95izPCyzmrc-unsplash.jpg')";
+
+    // DOMelements.welcomeBanner.style.backgroundImage = bgImg;
 };
 
 export {mainPageContent};
